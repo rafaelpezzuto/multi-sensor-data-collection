8b878489f48aa1bdfe3d8c777520d0b8691055ac
Rafael JPD <0pbakgvg0oc4jmphjxnyg2kl6dz3j7@bots.bitbucket.org>
2023-10-17 17:07:21 -0300

    Atualiza deps

--- app/src/main/java/org/rjpd/data/SettingsActivity.kt
+++ app/src/main/java/org/rjpd/data/SettingsActivity.kt
@@ -1,9 +1,17 @@
 package org.rjpd.data
 
+import android.content.Context
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -23,6 +31,82 @@ class SettingsActivity : AppCompatActivity() {
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.user_preferences, rootKey)
+
+//            val cameraManager = requireContext().getSystemService(Context.CAMERA_SERVICE) as CameraManager
+//            setCameras(cameraManager)
+        }
+
+        private fun setCameras(cameraManager: CameraManager) {
+            val cameraConfigurations = ArrayList<CameraConfiguration>()
+
+            for (cameraId in cameraManager.cameraIdList) {
+                val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+                val sizes = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)?.getOutputSizes(
+                    SurfaceTexture::class.java)
+                val fpsRanges = characteristics.get(CameraCharacteristics.CONTROL_AE_AVAILABLE_TARGET_FPS_RANGES)
+                val lensFacing = characteristics.get(CameraCharacteristics.LENS_FACING)
+
+                for (i in sizes!!.indices) {
+                    val size = sizes[i]
+                    val width = size.width
+                    val height = size.height
+
+                    try {
+                        val fpsRange = fpsRanges?.get(i)
+                        val averageFps = (fpsRange!!.lower + fpsRange.upper) / 2
+
+                        val cc = CameraConfiguration(
+                            cameraId = cameraId,
+                            resolutionWidth = width,
+                            resolutionHeight = height,
+                            averageFps = averageFps,
+                            lensFacing = lensFacing!!)
+
+                        cameraConfigurations.add(cc)
+                        Log.d("MultiSensorDataCollector", cc.getLabel())
+
+                    } catch (exc: ArrayIndexOutOfBoundsException) {
+                        //
+                    }
+                }
+            }
+
+            val cameraPreference = findPreference<ListPreference>("camera")
+            cameraPreference?.entries = cameraConfigurations.map { it.getLabel() }.toTypedArray()
+            cameraPreference?.entryValues = cameraConfigurations.map { it.getUniqueId() }.toTypedArray()
+
+            cameraPreference?.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { preference, newValue ->
+                val sharedPreferences = preference.sharedPreferences
+                val editor = sharedPreferences?.edit()
+
+                val cameraConfigurationSelected = cameraConfigurations.find{it.getUniqueId() == newValue as String}
+                editor?.putString("camera_selected", cameraConfigurationSelected!!.getUniqueId())
+                editor?.putString("camera_id", cameraConfigurationSelected!!.cameraId)
+                editor?.putInt("camera_fps", cameraConfigurationSelected!!.averageFps)
+                editor?.putInt("camera_resolution_width", cameraConfigurationSelected!!.resolutionWidth)
+                editor?.putInt("camera_resolution_height", cameraConfigurationSelected!!.resolutionHeight)
+                editor?.putInt("camera_lens_facing", cameraConfigurationSelected!!.lensFacing)
+
+                preference.summary = cameraConfigurationSelected!!.getLabel()
+                editor?.apply()
+
+                true
+            }
+
+            val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext().applicationContext)
+            if (sharedPreferences.getString("camera", null) == null) {
+                try {
+                    val cameraIndex = cameraConfigurations.indexOfFirst {
+                        it.resolutionWidth == 1920
+                                && it.resolutionHeight == 1080
+                                && it.averageFps == 30
+                                && it.lensFacing == CameraCharacteristics.LENS_FACING_BACK
+                    }
+                    cameraPreference?.setValueIndex(cameraIndex)
+                } catch (exc: Exception) {
+                    cameraPreference?.setValueIndex(0)
+                }
+            }
         }
     }
 
--- app/src/main/java/org/rjpd/data/Utils.kt
+++ app/src/main/java/org/rjpd/data/Utils.kt
@@ -1,12 +1,16 @@
 package org.rjpd.data
 
+import android.util.Log
 import java.io.BufferedWriter
 import java.io.File
+import java.io.FileOutputStream
 import java.io.FileWriter
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 
 
 fun createSubDirectory(rootDirectory: String, subDirectory: String): File {
@@ -20,9 +24,10 @@ fun createSubDirectory(rootDirectory: String, subDirectory: String): File {
     return directory
 }
 
-fun moveContent(sourceDirOrFile: File, destDir: File) {
+fun moveContent(sourceDirOrFile: File, destDir: File): Boolean {
     if (!sourceDirOrFile.exists()) {
-        return
+        Log.d("MultiSensorDataCollector", "$sourceDirOrFile does not exist")
+        return false
     }
 
     if (!destDir.exists()) {
@@ -32,6 +37,7 @@ fun moveContent(sourceDirOrFile: File, destDir: File) {
     if (sourceDirOrFile.isDirectory) {
         val files = sourceDirOrFile.listFiles()
         for (file in files) {
+            Log.d("MultiSensorDataCollector", "Moving file ${file.absolutePath}")
             val destFile = File(destDir, file.name)
             if (file.isDirectory) {
                 moveContent(file, destFile)
@@ -43,6 +49,46 @@ fun moveContent(sourceDirOrFile: File, destDir: File) {
         val destFile = File(destDir, sourceDirOrFile.name)
         sourceDirOrFile.renameTo(destFile)
     }
+
+    return true
+}
+
+fun zipFolder(sourceFolder: File, destinationZipFile: File) {
+    val fos = FileOutputStream(destinationZipFile)
+    val zos = ZipOutputStream(fos)
+    zos.setMethod(ZipOutputStream.DEFLATED)
+
+    zipDirectory(sourceFolder, sourceFolder, zos)
+
+    zos.close()
+    fos.close()
+}
+
+fun zipDirectory(baseDir: File, sourceFolder: File, zos: ZipOutputStream) {
+    val files = sourceFolder.listFiles() ?: return
+
+    for (file in files) {
+        if (file.isDirectory) {
+            zipDirectory(baseDir, File(sourceFolder, file.name), zos)
+        } else {
+            val entry = ZipEntry(baseDir.toURI().relativize(file.toURI()).path)
+            zos.putNextEntry(entry)
+
+            val fileInputStream = file.inputStream()
+            fileInputStream.copyTo(zos)
+            fileInputStream.close()
+            zos.closeEntry()
+        }
+    }
+}
+
+fun getZipTargetFile(currentOutputDir: File): File {
+    val s = currentOutputDir.parent
+    val f = currentOutputDir.name
+
+    val destinationZipFile = File(s, "${f}.zip")
+
+    return destinationZipFile
 }
 
 fun writeGeolocationData(
