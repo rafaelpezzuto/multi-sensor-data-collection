485f160c63514f63800f406e84b59bb8b694e727
Rafael JPD <0pbakgvg0oc4jmphjxnyg2kl6dz3j7@bots.bitbucket.org>
2023-10-21 18:18:18 -0300

    Rename Utils to FileUtils and add a writeMetadataFile method

---
+++ app/src/main/java/org/rjpd/data/FileUtils.kt
@@ -0,0 +1,154 @@
+package org.rjpd.data
+
+import android.os.Build
+import android.util.DisplayMetrics
+import android.util.Log
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import org.json.JSONObject
+
+
+fun createSubDirectory(rootDirectory: String, subDirectory: String): File {
+    val directory = File(
+        rootDirectory,
+        subDirectory
+    )
+    if (!directory.exists()) {
+        directory.mkdirs()
+    }
+    return directory
+}
+
+fun moveContent(sourceDirOrFile: File, destDir: File): Boolean {
+    if (!sourceDirOrFile.exists()) {
+        Log.d("FileUtils", "$sourceDirOrFile does not exist")
+        return false
+    }
+
+    if (!destDir.exists()) {
+        destDir.mkdirs()
+    }
+
+    if (sourceDirOrFile.isDirectory) {
+        val files = sourceDirOrFile.listFiles()
+        for (file in files) {
+            Log.d("FileUtils", "Moving file ${file.absolutePath}")
+            val destFile = File(destDir, file.name)
+            if (file.isDirectory) {
+                moveContent(file, destFile)
+            } else {
+                file.renameTo(destFile)
+            }
+        }
+    } else {
+        val destFile = File(destDir, sourceDirOrFile.name)
+        sourceDirOrFile.renameTo(destFile)
+    }
+
+    return true
+}
+
+fun getZipTargetFilename(currentOutputDir: File): String {
+    val s = currentOutputDir.parent
+    val f = currentOutputDir.name
+
+    val destinationZipFile = File(s, "${f}.zip")
+
+    return destinationZipFile.absolutePath
+}
+
+fun zipEverything(sourceDir: File, targetZipFilename: String) {
+    // ToDo: method responsible for compressing everything inside the directory sourceDir
+}
+
+fun writeGeolocationData(
+    gpsInterval: String,
+    accuracy: String,
+    latitude: String,
+    longitude: String,
+    outputDir: String,
+    filename: String,
+) {
+    val localeDate = SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format(Date())
+
+    val line = "$localeDate,$gpsInterval,$accuracy,$latitude,$longitude\n"
+
+    try {
+        val file = File(outputDir, "${filename}.gps.csv")
+        val writer = BufferedWriter(FileWriter(file, true))
+        writer.append(line)
+        writer.close()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
+
+fun writeSensorData(
+    name: String?,
+    axisData: String?,
+    timestamp: Long?,
+    accuracy: Int?,
+    outputDir: String,
+    filename: String,
+) {
+    val fmtDate = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS", Locale.getDefault()).format(Date())
+
+    val line = "$fmtDate,$name,$axisData,$timestamp,$accuracy\n"
+
+    try {
+        val file = File(outputDir, "${filename}.sensors.csv")
+        val writer = BufferedWriter(FileWriter(file, true))
+        writer.append(line)
+        writer.close()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
+
+fun writeMetadataFile(
+    preferencesData: MutableMap<String, *>,
+    displayMetrics: DisplayMetrics,
+    sensorsData: Map<String, Any>,
+    cameraConfigurationsData: ArrayList<CameraConfiguration>,
+    startDatetime: String,
+    stopDatetime: String,
+    outputDir: File,
+    filename: String,
+) {
+    val metadata = mutableMapOf<String, Any>()
+    metadata["preferences"] = preferencesData.toMutableMap()
+
+    metadata["start_time"] = startDatetime
+    metadata["stop_time"] = stopDatetime
+
+    metadata["device"] = mutableMapOf(
+        "model" to Build.MODEL,
+        "manufacturer" to Build.MANUFACTURER,
+        "android_version" to Build.VERSION.SDK_INT,
+        "screen" to mutableMapOf(
+            "screenWidthPixels" to displayMetrics.widthPixels,
+            "screenHeightPixels" to displayMetrics.heightPixels,
+            "screenDensity" to displayMetrics.density,
+            "screenDpi" to displayMetrics.densityDpi,
+        ),
+        "sensors" to sensorsData.toMutableMap(),
+        "cameras" to cameraConfigurationsData.map { it.toString() },
+    )
+
+    val metadataString = JSONObject(metadata as Map<*, *>?).toString()
+    Log.d("FileUtils", metadataString)
+
+    try {
+        val file = File(outputDir, "${filename}.metadata.json")
+        val writer = BufferedWriter(FileWriter(file, false))
+        writer.write(metadataString)
+        writer.close()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+}
