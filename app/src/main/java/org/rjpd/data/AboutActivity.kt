22307963a211e92443c6ba51f7cca578f948809e
Rafael JPD <0pbakgvg0oc4jmphjxnyg2kl6dz3j7@bots.bitbucket.org>
2023-10-22 22:02:42 -0300

    Create an about-us activity

---
+++ app/src/main/java/org/rjpd/data/AboutActivity.kt
@@ -0,0 +1,17 @@
+package org.rjpd.data
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+
+        val buttonClose = findViewById<Button>(R.id.close_button)
+        buttonClose.setOnClickListener {
+            finish()
+        }
+    }
+}
