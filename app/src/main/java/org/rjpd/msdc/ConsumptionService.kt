6fb1f6017b3493d23a9b460a8f20be57a450fda2
Rafael JPD <0pbakgvg0oc4jmphjxnyg2kl6dz3j7@bots.bitbucket.org>
2023-10-23 18:42:13 -0300

    Add a consumption monitoring service

---
+++ app/src/main/java/org/rjpd/msdc/ConsumptionService.kt
@@ -0,0 +1,63 @@
+package org.rjpd.msdc
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.BatteryManager
+import android.os.IBinder
+import android.util.Log
+import androidx.preference.PreferenceManager
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.TimeUnit
+
+class ConsumptionService : Service() {
+    private lateinit var scheduledExecutor: ScheduledExecutorService
+    private var isMonitoring = false
+
+    private var filename = ""
+    private var outputDir = ""
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (!isMonitoring) {
+            startMonitoring(intent)
+        }
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    private fun startMonitoring(intent: Intent?) {
+        isMonitoring = true
+
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val consumptionInterval = sharedPreferences.getInt("consumption_interval", 15).toLong()
+
+        outputDir = intent?.extras!!.getString("outputDirectory", "")
+        filename = intent?.extras!!.getString("filename", "")
+
+        val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+
+        scheduledExecutor = Executors.newSingleThreadScheduledExecutor()
+        scheduledExecutor.scheduleAtFixedRate({
+            val currentTime = System.currentTimeMillis()
+            val batteryStatus = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
+
+            writeConsumptionData(currentTime, batteryStatus, outputDir, filename)
+            Log.d("ConsumptionService", "$currentTime,$batteryStatus")
+
+        }, 0, consumptionInterval, TimeUnit.SECONDS)
+    }
+
+    override fun onDestroy() {
+        stopMonitoring()
+        super.onDestroy()
+    }
+
+    private fun stopMonitoring() {
+        isMonitoring = false
+        scheduledExecutor.shutdown()
+    }
+}
