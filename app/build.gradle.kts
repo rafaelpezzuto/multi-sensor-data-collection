e1feb8ba2ba89d84e1b7372a2758b01f92ecc531
Rafael JPD <0pbakgvg0oc4jmphjxnyg2kl6dz3j7@bots.bitbucket.org>
2023-12-31 19:10:53 -0300

    Add dependency jodadatetime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.play:integrity:1.3.0")
+    implementation("net.danlew:android.joda:2.12.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
