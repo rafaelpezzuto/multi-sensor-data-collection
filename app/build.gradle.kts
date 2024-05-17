c1e3c472ddd8fdb56f1b9b05dff86f462ffda7eb
Rafael JPD <rafael.pezzuto@gmail.com>
2024-05-17 14:35:52 -0300

    update version to 0.9.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.rjpd.msdc"
         minSdk = 26
         targetSdk = 33
-        versionCode = 28
-        versionName = "0.9.5.1"
+        versionCode = 29
+        versionName = "0.9.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
