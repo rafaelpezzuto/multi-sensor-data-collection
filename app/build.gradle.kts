eb34e192aa749dd8f852a8df7457d2b3288f4869
Rafael JPD <rafael.pezzuto@gmail.com>
2024-08-28 13:46:46 -0300

    add dependency for interacting with USB devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.test:core:1.6.1")
     implementation("androidx.test:rules:1.6.1")
     implementation("org.chromium.net:cronet-embedded:119.6045.31")
+    implementation("com.github.mik3y:usb-serial-for-android:3.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
