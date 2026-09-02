89b82ff94bdf98c58fc508e7309e7591f829021c
Rafael Damaceno <rafael.pezzuto@gmail.com>
2026-09-01 22:29:49 -0300

    Add osmdroid map dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.joda.time)
     implementation(libs.timber)
     implementation(libs.usb.serial)
+    implementation(libs.osmdroid.android)
 
     testImplementation(libs.junit)
 
